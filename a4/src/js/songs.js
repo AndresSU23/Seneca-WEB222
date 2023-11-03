3a577f9f7ae212cb8008761ca537c1f23e4b1671
AndresSU23 <111714730+AndresSU23@users.noreply.github.com>
2023-11-03 01:47:07 -0400

    Assignmet 4 FInished Verified

---
+++ a4/src/js/songs.js
@@ -0,0 +1,179 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    songId: "SID-00001",
+    artistId: "AID-001",
+    title: "Dia Tras Dia",
+    year: "2009",
+    duration: 245,
+    url: "https://youtu.be/NEw4ZUbpfsw?si=mcSBiGraj0hxVftl",
+    explicit: false
+  },
+  {
+    songId: "SID-00002",
+    artistId: "AID-001",
+    title: "Besos Usados",
+    year: "2009",
+    duration: 234,
+    url: "https://youtu.be/4TzSai4xw4o?si=XhctWYJUQzfiqZ-x",
+    explicit: true
+  },
+  {
+    songId: "SID-00003",
+    artistId: "AID-002",
+    title: "Limon y Sal",
+    year: "2006",
+    duration: 205,
+    url: "https://youtu.be/tIpzfs5tBJU?si=43ZJKSVXCJjYCEbo",
+    explicit: false
+  },
+  {
+    songId: "SID-00004",
+    artistId: "AID-002",
+    title: "Me Voy",
+    year: "2006",
+    duration: 187,
+    url: "https://youtu.be/y8rBC6GCUjg?si=GaY_it7getViVrB-",
+    explicit: false
+  },
+  {
+    songId: "SID-00005",
+    artistId: "AID-002",
+    title: "Eres para mi",
+    year: "2006",
+    duration: 187,
+    url: "https://youtu.be/pj2ntDiXJCk?si=CT1_blrdWY7k1OJZ",
+    explicit: false
+  },
+  {
+    songId: "SID-00006",
+    artistId: "AID-003",
+    title: "A Dios le pido",
+    year: "2002",
+    duration: 205,
+    url: "https://youtu.be/kMIaYXxLnUA?si=xvRX5N_UaEWWIw8d",
+    explicit: false
+  },
+  {
+    songId: "SID-00007",
+    artistId: "AID-003",
+    title: "Me Enamora",
+    year: "2007",
+    duration: 192,
+    url: "https://youtu.be/voxgN3Dhjuo?si=-pO6DqDuIFueauxY",
+    explicit: false
+  },
+  {
+    songId: "SID-00008",
+    artistId: "AID-003",
+    title: "Camisa Negra",
+    year: "2004",
+    duration: 216,
+    url: "https://youtu.be/kRt2sRyup6A?si=v152nSxAt4mBTrl8",
+    explicit: true
+  },
+  {
+    songId: "SID-00009",
+    artistId: "AID-004",
+    title: "Tu Falta de Querer",
+    year: "2015",
+    duration: 278,
+    url: "https://youtu.be/WT-VE9OyAJk?si=vPRxIzdZuZgfzycN",
+    explicit: false
+  },
+  {
+    songId: "SID-00010",
+    artistId: "AID-004",
+    title: "Mi Buen Amor",
+    year: "2018",
+    duration: 227,
+    url: "https://youtu.be/13m9v78uNJk?si=XurdtPDXq_LVXZo0",
+    explicit: false
+  },
+  {
+    songId: "SID-00011",
+    artistId: "AID-005",
+    title: "La Tortura",
+    year: "2005",
+    duration: 212,
+    url: "https://youtu.be/Dsp_8Lm1eSk?si=rjOCmZa_aVAFdWDo",
+    explicit: false
+  },
+  {
+    songId: "SID-00012",
+    artistId: "AID-006",
+    title: "Te Mando Flores",
+    year: "2005",
+    duration: 228,
+    url: "https://youtu.be/RDkeRUNtFQY?si=PgP4yxcT51kYQCVU",
+    explicit: false
+  },
+  {
+    songId: "SID-00013",
+    artistId: "AID-007",
+    title: "Novio del Año",
+    year: "2023",
+    duration: 154,
+    url: "https://youtu.be/xccGahYACPI?si=b1ACkudQF-l_b6-n",
+    explicit: false
+  },
+  {
+    songId: "SID-00014",
+    artistId: "AID-008",
+    title: "Lamento Boliviano",
+    year: "1994",
+    duration: 214,
+    url: "https://youtu.be/xccGahYACPI?si=b1ACkudQF-l_b6-n",
+    explicit: false
+  },
+  {
+    songId: "SID-00015",
+    artistId: "AID-009",
+    title: "Don",
+    year: "2007",
+    duration: 183,
+    url: "https://youtu.be/eap0G9ldKc0?si=_01-LY4faNhvSorV",
+    explicit: true
+  },
+  {
+    songId: "SID-00016",
+    artistId: "AID-010",
+    title: "Besos en Guerra",
+    year: "2018",
+    duration: 231,
+    url: "https://youtu.be/1oeD2m2UQAI?si=B-qnI3WDSpFLQj8q",
+    explicit: false
+  },
+  {
+    songId: "SID-00017",
+    artistId: "AID-010",
+    title: "Como Te Atreves A Volver",
+    year: "2016",
+    duration: 240,
+    url: "https://youtu.be/_gm5piKnrS4?si=xkcO8c_vm55N2q4L",
+    explicit: false
+  },
+  {
+    songId: "SID-00019",
+    artistId: "AID-011",
+    title: "Conquistar el Planeta",
+    year: "2023",
+    duration: 163,
+    url: "https://youtu.be/4iu-2-yvhs0?si=rF1C5RJCAYi_kwja",
+    explicit: false
+  },
+  {
+    songId: "SID-00020",
+    artistId: "AID-012",
+    title: "Bailar Contigo",
+    year: "2018",
+    duration: 223,
+    url: "https://youtu.be/mT7_qIFBa3Y?si=DBOA21E9gYRNWaVJ",
+    explicit: false
+  }
+];
