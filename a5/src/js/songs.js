5d2090d0fe7709dfcf24e862c02b817927adfa86
AndresSU23 <111714730+AndresSU23@users.noreply.github.com>
2023-11-27 21:23:19 -0500

    Assignment 5

---
+++ a5/src/js/songs.js
@@ -0,0 +1,226 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    songId: "SID-00001",
+    artistId: "AID-001",
+    title: "Dia Tras Dia",
+    year: "2009",
+    duration: 245,
+    url: "https://youtu.be/NEw4ZUbpfsw?si=mcSBiGraj0hxVftl",
+    explicit: false,
+    image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Andr%C3%A9s_Cepeda_guitarra.jpg/320px-Andr%C3%A9s_Cepeda_guitarra.jpg",
+    image_ref: "https://commons.wikimedia.org/wiki/File:Andr%C3%A9s_Cepeda_guitarra.jpg"
+  },
+  {
+    songId: "SID-00002",
+    artistId: "AID-001",
+    title: "Besos Usados",
+    year: "2009",
+    duration: 234,
+    url: "https://youtu.be/4TzSai4xw4o?si=XhctWYJUQzfiqZ-x",
+    explicit: true,
+    image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Andr%C3%A9s_Cepeda_guitarra.jpg/320px-Andr%C3%A9s_Cepeda_guitarra.jpg",
+    image_ref: "https://commons.wikimedia.org/wiki/File:Andr%C3%A9s_Cepeda_guitarra.jpg"
+  },
+  {
+    songId: "SID-00003",
+    artistId: "AID-002",
+    title: "Limon y Sal",
+    year: "2006",
+    duration: 205,
+    url: "https://youtu.be/tIpzfs5tBJU?si=43ZJKSVXCJjYCEbo",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/2348/2191495539_f7432c6841_w.jpg",
+    image_ref: "https://www.flickr.com/photos/foteropanico/2191495539/"
+  },
+  {
+    songId: "SID-00004",
+    artistId: "AID-002",
+    title: "Me Voy",
+    year: "2006",
+    duration: 187,
+    url: "https://youtu.be/y8rBC6GCUjg?si=GaY_it7getViVrB-",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/4192/34258559626_384e98937d_w.jpg",
+    image_ref: "https://www.flickr.com/photos/donostiakultura/34258559626"
+  },
+  {
+    songId: "SID-00005",
+    artistId: "AID-002",
+    title: "Eres para mi",
+    year: "2006",
+    duration: 187,
+    url: "https://youtu.be/pj2ntDiXJCk?si=CT1_blrdWY7k1OJZ",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/2806/34258562886_a9d3fe19eb_w.jpg",
+    image_ref: "https://www.flickr.com/photos/donostiakultura/34258562886"
+  },
+  {
+    songId: "SID-00006",
+    artistId: "AID-003",
+    title: "A Dios le pido",
+    year: "2002",
+    duration: 205,
+    url: "https://youtu.be/kMIaYXxLnUA?si=xvRX5N_UaEWWIw8d",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/65535/47637460882_650395c613_w.jpg",
+    image_ref: "https://www.flickr.com/photos/elhormiguerotv/47637460882"
+  },
+  {
+    songId: "SID-00007",
+    artistId: "AID-003",
+    title: "Me Enamora",
+    year: "2007",
+    duration: 192,
+    url: "https://youtu.be/voxgN3Dhjuo?si=-pO6DqDuIFueauxY",
+    explicit: false,
+    image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Juanes_2008.06.25_010.jpg/320px-Juanes_2008.06.25_010.jpg",
+    image_ref: "https://commons.wikimedia.org/wiki/File:Juanes_2008.06.25_010.jpg"
+  },
+  {
+    songId: "SID-00008",
+    artistId: "AID-003",
+    title: "Camisa Negra",
+    year: "2004",
+    duration: 216,
+    url: "https://youtu.be/kRt2sRyup6A?si=v152nSxAt4mBTrl8",
+    explicit: true,
+    image_url: "https://live.staticflickr.com/65535/47637460882_650395c613_w.jpg",
+    image_ref: "https://www.flickr.com/photos/elhormiguerotv/47637460882"
+  },
+  {
+    songId: "SID-00009",
+    artistId: "AID-004",
+    title: "Tu Falta de Querer",
+    year: "2015",
+    duration: 278,
+    url: "https://youtu.be/WT-VE9OyAJk?si=vPRxIzdZuZgfzycN",
+    explicit: false,
+    image_url:
+      "https://images.pexels.com/photos/15603601/pexels-photo-15603601/free-photo-of-mon-laferte-en-parque-mexico.jpeg?auto=compress&cs=tinysrgb&w=200&h=300&dpr=1",
+    image_ref: "https://www.pexels.com/photo/mon-laferte-en-parque-mexico-15603601/"
+  },
+  {
+    songId: "SID-00010",
+    artistId: "AID-004",
+    title: "Mi Buen Amor",
+    year: "2018",
+    duration: 227,
+    url: "https://youtu.be/13m9v78uNJk?si=XurdtPDXq_LVXZo0",
+    explicit: false,
+    image_url:
+      "https://images.pexels.com/photos/15603601/pexels-photo-15603601/free-photo-of-mon-laferte-en-parque-mexico.jpeg?auto=compress&cs=tinysrgb&w=200&h=300&dpr=1",
+    image_ref: "https://www.pexels.com/photo/mon-laferte-en-parque-mexico-15603601/"
+  },
+  {
+    songId: "SID-00011",
+    artistId: "AID-005",
+    title: "La Tortura",
+    year: "2005",
+    duration: 212,
+    url: "https://youtu.be/Dsp_8Lm1eSk?si=rjOCmZa_aVAFdWDo",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/589/32328233426_945db50b3a_w.jpg",
+    image_ref: "https://www.flickr.com/photos/worldeconomicforum/32328233426"
+  },
+  {
+    songId: "SID-00012",
+    artistId: "AID-006",
+    title: "Te Mando Flores",
+    year: "2005",
+    duration: 228,
+    url: "https://youtu.be/RDkeRUNtFQY?si=PgP4yxcT51kYQCVU",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/1319/748656486_a2ec58b869_w.jpg",
+    image_ref: "https://www.flickr.com/photos/mejiaperalta/748656486"
+  },
+  {
+    songId: "SID-00013",
+    artistId: "AID-007",
+    title: "Novio del Año",
+    year: "2023",
+    duration: 154,
+    url: "https://youtu.be/xccGahYACPI?si=b1ACkudQF-l_b6-n",
+    explicit: false,
+    image_url:
+      "https://images.unsplash.com/photo-1579449240558-efe5e1e720c7?&w=200&h=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image_ref:
+      "https://unsplash.com/photos/person-in-brown-sweater-showing-middle-finger-3iqiKi3Dh-E"
+  },
+  {
+    songId: "SID-00014",
+    artistId: "AID-008",
+    title: "Lamento Boliviano",
+    year: "1994",
+    duration: 214,
+    url: "https://youtu.be/xccGahYACPI?si=b1ACkudQF-l_b6-n",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/65535/48059193468_e5924bf576_w.jpg",
+    image_ref: "https://www.flickr.com/photos/julioenriquez/48059193468/"
+  },
+  {
+    songId: "SID-00015",
+    artistId: "AID-009",
+    title: "Don",
+    year: "2007",
+    duration: 183,
+    url: "https://youtu.be/eap0G9ldKc0?si=_01-LY4faNhvSorV",
+    explicit: true,
+    image_url:
+      "https://images.unsplash.com/photo-1574634534894-89d7576c8259?&w=200&h=300&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image_ref: "https://unsplash.com/photos/woman-holding-bag-beside-mural-painting-7qCeFo19r24"
+  },
+  {
+    songId: "SID-00016",
+    artistId: "AID-010",
+    title: "Besos en Guerra",
+    year: "2018",
+    duration: 231,
+    url: "https://youtu.be/1oeD2m2UQAI?si=B-qnI3WDSpFLQj8q",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/7844/40453959033_b296d26fc4_w.jpg",
+    image_ref: "https://www.flickr.com/photos/casamerica/40453959033"
+  },
+  {
+    songId: "SID-00017",
+    artistId: "AID-010",
+    title: "Como Te Atreves A Volver",
+    year: "2016",
+    duration: 240,
+    url: "https://youtu.be/_gm5piKnrS4?si=xkcO8c_vm55N2q4L",
+    explicit: false,
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Morat_logo.png?20170805071736",
+    image_ref: "https://commons.wikimedia.org/wiki/File:Morat_logo.png"
+  },
+  {
+    songId: "SID-00019",
+    artistId: "AID-011",
+    title: "Conquistar el Planeta",
+    year: "2023",
+    duration: 163,
+    url: "https://youtu.be/4iu-2-yvhs0?si=rF1C5RJCAYi_kwja",
+    explicit: false,
+    image_url:
+      "https://images.unsplash.com/photo-1519500099198-fd81846b8f03?&w=200&h=300&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image_ref: "https://unsplash.com/photos/blue-earth-globe-on-table-4JL_VAgxwcU"
+  },
+  {
+    songId: "SID-00020",
+    artistId: "AID-012",
+    title: "Bailar Contigo",
+    year: "2018",
+    duration: 223,
+    url: "https://youtu.be/mT7_qIFBa3Y?si=DBOA21E9gYRNWaVJ",
+    explicit: false,
+    image_url: "https://live.staticflickr.com/7856/33664128778_463a8a4ff4_w.jpg",
+    image_ref: "https://www.flickr.com/photos/isengard/33664128778/"
+  }
+];
